c550049392e51597d8e54d1540fd35e79231e973
Alex Karpov <keyfour13@gmail.com>
2017-07-05 16:26:50 +0300

    Initial commit

---
+++ app/src/main/java/com/github/keyfour/currencyexchangerate/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.keyfour.currencyexchangerate;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Main {@link android.app.Activity} of application
+ *
+ * @author Alex Karpov
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+}
