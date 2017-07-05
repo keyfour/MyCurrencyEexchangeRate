91c882e915dcd250103e7057686196e702638254
Alex Karpov <keyfour13@gmail.com>
2017-07-05 16:34:05 +0300

    Add inflation to activity and id to layout

--- app/src/main/java/com/github/keyfour/currencyexchangerate/MainActivity.java
+++ app/src/main/java/com/github/keyfour/currencyexchangerate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.keyfour.currencyexchangerate;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,6 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
+    }
 
 }
